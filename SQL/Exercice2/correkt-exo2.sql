ff87beeb8131dbbcf0bb34db3aa0bc018be92e5e
GabrielAbrDam <gabriel.abreudamaso@gmail.com>
2018-02-22 10:01:43 +0100

    corr 2exo

--- SQL/Exercice2/correkt-exo2.sql
+++ SQL/Exercice2/correkt-exo2.sql
@@ -1,1 +1,7 @@
-SELECT agent_code, sum(amount), count(id) FROM test.orders where amount >=1000 group by amount order by sum(amount) desc;
+use test;
+
+SELECT agent_code FROM orders WHERE amount >= 1000;
+SELECT agent_code, SUM(amount) FROM orders WHERE amount >= 1000 GROUP BY agent_code;
+SELECT agent_code, SUM(amount), count(id) FROM orders WHERE amount >= 1000 GROUP BY agent_code;
+SELECT agent_code, SUM(amount), count(id) FROM orders WHERE amount >= 1000 GROUP BY agent_code ORDER BY AVG(amount);
+SELECT agent_code, sum(amount), count(id) FROM orders where amount >= 1000 GROUP BY agent_code order by avg(amount) desc;
