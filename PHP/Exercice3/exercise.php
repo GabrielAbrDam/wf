8321e4368892d2dd0c685a669145edac4f6ddb84
GabrielAbrDam <gabriel.abreudamaso@gmail.com>
2018-02-27 15:57:42 +0100

    my ex 3

--- PHP/Exercice3/exercise.php
+++ PHP/Exercice3/exercise.php
@@ -1,4 +1,24 @@
 <?php
+var_dump($input);
+
+$aResult = 0;
+$bResult = 0;
+
+foreach($input as $turn){
+list($a, $b) = $turn;
+
+if($a > $b){
+    $aResult++;
+}else if($b > $a){
+    $bResult++;
+    }
+}
+
+if($aResult > $bResult){
+    $winner = 'A';
+}else{
+    $winner = 'B';
+}
 
 $input;
 $winner;
