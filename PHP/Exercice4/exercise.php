b46f8b2f3dd2ff05f5996f978a0d86e21b0a274e
GabrielAbrDam <gabriel.abreudamaso@gmail.com>
2018-02-28 10:41:37 +0100

    My exo4 PHP

--- PHP/Exercice4/exercise.php
+++ PHP/Exercice4/exercise.php
@@ -1,1 +1,18 @@
 <?php
+
+function getAllMondaysOfMonth(int $year, int $month):array{
+    $mondays = [];
+    $currentMonth = DateTime::createFromFormat('Ym' , $year . $month);
+    $firstMonday = new DateTime('first monday of ' . $currentMonth->format('F Y'));
+    $interval = DateInterval::createFromDateString('next monday');
+    
+    $lastMonday = new DateTime('last monday of ' . $currentMonth->format('F Y'));
+ 
+    while($firstMonday <= $lastMonday){
+        $mondays[] = $firstMonday->format('l j, M Y');
+        $firstMonday->add($interval);
+    }
+    var_dump($mondays);
+    return $mondays;
+}
+
