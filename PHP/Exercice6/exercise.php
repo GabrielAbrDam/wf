3a0a93871c1c0be15eefc8ff72dbaf488ffe2742
GabrielAbrDam <gabriel.abreudamaso@gmail.com>
2018-02-28 16:55:35 +0100

    my php exo 6

--- PHP/Exercice6/exercise.php
+++ PHP/Exercice6/exercise.php
@@ -1,1 +1,21 @@
 <?php
+function easterReverse($coucou){
+    $fPath = fopen($coucou, 'r+');
+    $fileContent = fread($fPath, filesize($coucou)); 
+    $positionInitial = floor(strlen($fileContent)/2);
+    fseek($fPath, $positionInitial, SEEK_SET);
+    $secondPart = fread($fPath, filesize($coucou));
+    $reversedText = strrev($secondPart);
+    
+    fseek($fPath, $positionInitial, SEEK_SET);
+    fwrite($fPath, $reversedText);
+    fclose($fPath); 
+
+   
+};
+
+
+
+//(strlen($fileContent) / 2
+//(strlen($fileContent) / 2;
+//$fseek($fPath, 0, SEEK_END)
