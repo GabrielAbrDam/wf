2838e0b2d8222637185e4bb9cdd05e460831ce68
GabrielAbrDam <gabriel.abreudamaso@gmail.com>
2018-03-02 09:41:36 +0100

    php my exercice 7

--- PHP/Exercice7/exercise.php
+++ PHP/Exercice7/exercise.php
@@ -1,1 +1,22 @@
 <?php
+function divide(int $value, int $par) : float{
+    if ($par == 0) {
+        throw new RuntimeException('Division by 0 is not allowed');
+    }
+    return (float) $value / $par;
+}
+
+function arrayDivide(array $values, int $divisor) : array {
+    $myArray=[];
+    
+    try{
+        foreach($values as $value){
+           $myArray[] = divide($value, $divisor);
+        } 
+        return $myArray;
+    } catch(RuntimeException $error){
+        echo $error->getMessage();
+        return $values;
+    }
+        
+}
